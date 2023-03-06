5aee4c9e0d7d37de321bd78e9d2446a3ec3d250b
mggaray <maty1173@gmail.com>
2023-03-06 16:51:08 -0300

    (fix) deploy error

---
+++ src/Services/credentials.ts
@@ -0,0 +1,16 @@
+interface Icredentials {
+  CLIENT_ID: string;
+  CLIENT_SECRET: string;
+  REDIRECT_URI: string;
+  AUTH_ENDPOINT: string;
+  RESPONSE_TYPE: string;
+}
+
+export const credentials: Icredentials = {
+  CLIENT_ID: "9ce584ba520242df94bd8fa9ba33d4cd",
+  CLIENT_SECRET: "3521d09a62ac463b93757a5e4b4e1e07",
+  //REDIRECT_URI: "https://spotify-searchmg.netlify.app/",
+  REDIRECT_URI: "http://localhost:5173",
+  AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+  RESPONSE_TYPE: "token",
+};
